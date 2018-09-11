3a5d7f9c6a1078bcf550ed8e63b1e066bb216a42
Gnedov Pavel <pavel@gnedov.info>
2018-09-11 23:04:33 +0700

    AppState moved into separated file

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,7 @@ import * as ReactDOM from 'react-dom';
 import {observable} from 'mobx';
 import {observer} from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
-
-class AppState {
-    @observable timer = 0;
-
-    constructor() {
-        setInterval(() => {
-            this.timer += 1;
-        }, 1000);
-    }
-
-    resetTimer() {
-        this.timer = 0;
-    }
-}
+import { AppState } from './app-state';
 
 @observer
 class TimerView extends React.Component<{appState: AppState}, {}> {
