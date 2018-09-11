b8f66c6d1f6bae528551a2bd760f9a24245ddeb8
Gnedov Pavel <pavel@gnedov.info>
2018-09-11 23:08:02 +0700

    Removed unused import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {observable} from 'mobx';
 import {observer} from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 import { appState, AppState } from './app-state';
